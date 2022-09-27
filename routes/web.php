355120dae835e94bd7c409d0939edcc3b933bbff
mauro-angel99 <angelottimauro@gmail.com>
2022-09-27 11:22:31 +0200

    laravel-auth

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Auth::routes();
+
+Route::get('/admin', 'Admin/HomeController@index')->middleware('auth')->name('admin.home');
+
+route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->group(function () {
+    route::get('/', 'HomeController@index')->name('home');
+});
+
+route::get("{any?}", function() {
+    return view('guest.home');
+})->where("any", ".*");
